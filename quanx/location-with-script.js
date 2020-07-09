ade0c2ee85e15f0684c24d3d861e694d144f3d3b
Daryl <25836646+srk24@users.noreply.github.com>
2020-07-09 16:33:34 +0800

    Update location-with-script.js

--- quanx/location-with-script.js
+++ quanx/location-with-script.js
@@ -1,7 +1,13 @@
 /**
- * @supportURL http://ip-api.com/json
+ * @version 0.0.1
+ * @author daryl
+ *
+ * @supported http://ip-api.com/json
  * @sample https://raw.githubusercontent.com/crossutility/Quantumult-X/master/sample-location-with-script.js
  */
+
+// $response, $notify(title, subtitle, message), console.log(message)
+// $response.statusCode, $response.headers, $response.body
 
 if ($response.statusCode !== 200) $done(Null)
 
