c107501d9839b95177a09ae6ed006b743f63b47a
Daryl <25836646+srk24@users.noreply.github.com>
2020-07-09 10:50:50 +0800

    Update dler_parser.js

--- dler_parser.js
+++ dler_parser.js
@@ -21,13 +21,13 @@ if ($resource.link.match(/dler/g)) {
   let l = $resource.content.split(/\n/g)
   for (let i = 0; i < l.length; i++) {
     let info = l[i].split(/tag=/g)
-    let array = info[1].replace(/新加坡/g, '狮城').replace(/高级|标准/g, '').split('>')
-    if (array.length === 2) {
-        info[1] = array[0] + '>' + array[1].substring(3)
+    let tag = info[1].replace(/新加坡/g, '狮城').replace(/高级|标准/g, '').split('>')
+    if (tag.length === 2) {
+        info[1] = tag[0] + '>' + tag[1].substring(3)
     } else {
-        info[1] = array[0]
+        info[1] = tag[0]
     }
-    l[i] = info[0] + info[1]
+    l[i] = info[0] + 'tag=' + info[1]
     $notify('info1','',l[i])
   }
   let resp = l.join('\n')
