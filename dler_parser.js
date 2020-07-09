7a1f5d2f49997f5dc74c4be896eecc1c48144a48
Daryl <25836646+srk24@users.noreply.github.com>
2020-07-09 10:47:49 +0800

    Update dler_parser.js

--- dler_parser.js
+++ dler_parser.js
@@ -1,4 +1,21 @@
-//2
+/**
+ * @author daryl
+ * @fileoverview Example to parse the resource to the format of Quantumult X.
+ * 
+ * @supported Quantumult X (v1.0.8-build253)
+ */
+
+
+// $resource, $notify(title, subtitle, message)
+// HTTP reqeust and persistent storage related APIs are not supported in resource parser.
+
+// $resource.link contains the original URL of the resource or the path if the resource is local.
+// $resource.content contains the response(UTF8) from the URL .
+
+// $done({error : "error description"});
+// $done({content : "the modified content"});
+
+
 if ($resource.link.match(/dler/g)) {
   let l = $resource.content.split(/\n/g)
   for (let i = 0; i < l.length; i++) {
