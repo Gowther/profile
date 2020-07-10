52a2087540f87a0e5932de48c1fa1b4b215d79b8
Daryl <25836646+srk24@users.noreply.github.com>
2020-07-10 11:47:42 +0800

    Update resource-parser.js

--- quanx/resource-parser.js
+++ quanx/resource-parser.js
@@ -1,7 +1,7 @@
 /**
  * 
  * @author daryl
- * @version 0.13
+ * @version 0.14
  * 
  * @supported Quantumult X (v1.0.8-build253)
  */
@@ -19,12 +19,13 @@ if (!$resource.content.match(/tag/g)) $done(Null)
 
 const sub_dler = /dler\.cloud\/subscribe/g
 const sub_zeichuan = /zeichuan\.vip\/link/g
+const reg_t = /tag=/g
 let list = $resource.content.split(/\n/g)
 
 if ($resource.link.match(sub_dler)) {
     for (let i = 0; i < list.length; i++) {
-        if (list[i].match(/tag/g)) {
-            let info = list[i].split(/tag=/g)
+        if (list[i].match(reg_t)) {
+            let info = list[i].split(reg_t)
             let tag = info[1].replace(/新加坡/g, '狮城').replace(/高级|标准/g, '').split('>')
             if (tag.length === 2) {
                 info[1] = tag[0] + '>' + tag[1].substring(3)
@@ -38,8 +39,8 @@ if ($resource.link.match(sub_dler)) {
     $done({ content: resp })
 } else if ($resource.link.match(sub_zeichuan)) {
     for (let i = 0; i < list.length; i++) {
-        if (list[i].match(/tag/g)) {
-            let info = list[i].split(/tag=/g)
+        if (list[i].match(reg_t)) {
+            let info = list[i].split(reg_t)
             info[1] = info[1].replace(/-/g, ' ').replace(/新加坡/g, '狮城')
             if (info[1].match(/〖多入口中转〗/g)) {
                 info[1] = info[1].replace(/〖多入口中转〗/g, ' ').concat(' CDN')
