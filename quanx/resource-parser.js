00a5d63a0b928a63603520ce7de6d38a0959c0bb
Daryl <25836646+srk24@users.noreply.github.com>
2020-07-27 15:07:36 +0800

    Update resource-parser.js

--- quanx/resource-parser.js
+++ quanx/resource-parser.js
@@ -1,7 +1,7 @@
 /**
  * 
  * @author daryl
- * @version 0.15
+ * @version 0.19
  * 
  * @supported Quantumult X (v1.0.8-build253)
  */
@@ -15,25 +15,17 @@
 // $done({error : "error description"});
 // $done({content : "the modified content"});
 
-if (!$resource.content.match(/tag/g)) $done(Null)
-
 const sub_dler = /dler\.cloud\/subscribe/g
 const reg_t = /tag=/g
 let list = $resource.content.split(/\n/g)
 
 if ($resource.link.match(sub_dler)) {
     for (let i = 0; i < list.length; i++) {
-        if (list[i].match(reg_t)) {
-            let info = list[i].split(reg_t)
-            let tag = info[1].replace(/高级|标准/g, '').replace(/新加坡/g, 'Singapore').replace(/香港/g, 'Hong Kong').split('>', 1)
-            if (tag.length === 2) {
-                info[1] = tag[0]
-            } else {
-                info[1] = tag[0]
-            }
-            list[i] = info[0] + 'tag=' + info[1]
-        }
+        let info = list[i].split(reg_t)
+        let tag = info[1].replace(/高级|标准/g, '').replace(/新加坡/g, 'Singapore').replace(/香港/g, 'Hong Kong').split('>', 1)
+        info[1] = tag[0]
+        list[i] = info[0] + 'tag=' + info[1]
     }
     let resp = list.join('\n')
     $done({ content: resp })
-} 
+}
