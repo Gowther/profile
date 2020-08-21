9ab59def42980b033699da43bfa71ef157eb3e21
Daryl <25836646+srk24@users.noreply.github.com>
2020-08-21 11:28:44 +0800

    Update resource-parser.js

--- quanx/resource-parser.js
+++ quanx/resource-parser.js
@@ -25,6 +25,7 @@ if ($resource.link.match(sub_dler)) {
         let tag = info[1].replace(/新加坡/g, 'Singapore').replace(/香港/g, 'Hong Kong').split(' - ', 1)
         info[1] = tag[0]
         list[i] = info[0] + 'tag=' + info[1]
+        $notify(i,'resp','resp')
     }
     $notify(list,'resp','resp')
     let resp = list.join('\n')
