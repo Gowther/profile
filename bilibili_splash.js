ffc8531bf88f958322ac9c847805644f28983651
Daryl <25836646+srk24@users.noreply.github.com>
2020-11-19 11:26:00 +0800

    Update bilibili_splash.js

--- bilibili_splash.js
+++ bilibili_splash.js
@@ -2,20 +2,15 @@
 
 let body = $response.body
 body = JSON.parse(body)
-if (body['data'].hasOwnProperty('max_time') && body['data'].hasOwnProperty('show')) {
-    body['data']['max_time'] = 0;
-    body['data']['min_interval'] = 31536000;
-    body['data']['pull_interval'] = 31536000;
-    body['data']['show']['stime'] = 1915027200;
-    body['data']['show']['etime'] = 1924272000;
-}
-if (body.hasOwnProperty('data') && body['data']['list']) {
-    for (let i = 0; i < body['data']['list'].length; i++) {
-        body['data']['list'][i]['duration'] = 0;
-        body['data']['list'][i]['begin_time'] = 1915027200;
-        body['data']['list'][i]['end_time'] = 1924272000;
+if(body.hasOwnProperty('data')){
+    if (body['data'].hasOwnProperty('max_time') && body['data'].hasOwnProperty('show')) {
+        body['data']['max_time'] = 0
+        body['data']['min_interval'] = 31536000
+        body['data']['pull_interval'] = 31536000
+        body['data']['show']['stime'] = 1915027200
+        body['data']['show']['etime'] = 1924272000
     }
-    body = JSON.stringify(body);
+    body['data']['list'] = new Array()
 }
 body = JSON.stringify(body)
 $done({ body })
