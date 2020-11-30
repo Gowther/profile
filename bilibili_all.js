03f05b0dfd6b57861edcd5ae10da68443f1bd038
Daryl <25836646+srk24@users.noreply.github.com>
2020-11-30 11:10:03 +0800

    Update bilibili_all.js

--- bilibili_all.js
+++ bilibili_all.js
@@ -44,28 +44,6 @@ if (url.includes('feed/index')) {
             }
         },
         {
-            "title": "创作中心",
-            "items": [
-                {
-                    "need_login": 1,
-                    "display": 1,
-                    "id": 171,
-                    "title": "创作首页",
-                    "global_red_dot": 1,
-                    "uri": "bilibili://uper/homevc",
-                    "icon": "http://i0.hdslb.com/bfs/archive/d3aad2d07538d2d43805f1fa14a412d7a45cc861.png"
-                },
-            ],
-            "style": 1,
-            "button": {
-                "icon": "http://i0.hdslb.com/bfs/archive/205f47675eaaca7912111e0e9b1ac94cb985901f.png",
-                "style": 1,
-                "url": "bilibili://uper/user_center/archive_selection",
-                "text": "发布"
-            }
-        },
-
-        {
             //"title": "其他设置",
             "items": [
                 {
