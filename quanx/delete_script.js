a5345e8a6b3d58fa5053a53d93669aeecfedf0e2
Daryl <25836646+srk24@users.noreply.github.com>
2020-12-02 00:05:03 +0800

    Update delete_script.js

--- quanx/delete_script.js
+++ quanx/delete_script.js
@@ -1,11 +1,25 @@
+ /**
+ * 删除 DivineEngine Rule 中的script的资源解析器
+ */
 // 删除 DivineEngine Rule 中的script的资源解析器
 
 const adv = 'https://raw.githubusercontent.com/DivineEngine/Profiles/master/Quantumult/Rewrite/Block/Advertising.conf'
 
+let list = $resource.content.split(/\n/g)
+
 if ($resource.link === adv) {
     $notify('1', 'sub', 'demo')
-    const regex = /# Script.*# URL Rewrite$/g
-    let resp = $resource.content
-    resp = content.replace(regex, '# URL Rewrite')
+    let startIndex
+    let endIndex
+    for (let i = 0; i < list.length; i++) {
+        if (list[i].includes('# Script')) {
+            startIndex = i
+        } else if (list[i].includes('# Rewrite')){
+            endIndex = i
+            break
+        }
+    }
+    list = list.splice(startIndex, (endIndex - startIndex));
+    let resp = list.join('\n')
     $done({ content: resp })
 }
