42b6e35424eac0e8885088aca5e89763db63a022
Daryl <25836646+srk24@users.noreply.github.com>
2021-08-19 10:58:47 +0800

    Update update_datashboard.bat

--- clash/update_datashboard.bat
+++ clash/update_datashboard.bat
@@ -4,8 +4,8 @@ echo ######################Step1  start clone clash dashboard...
 cd ./bin & if not exist clash-dashboard git clone https://github.com/Dreamacro/clash-dashboard.git -b gh-pages
 echo.
 
-echo ######################Step2  start pull...
-cd ./clash-dashboard & git pull
+echo ######################Step2  start update...
+cd ./clash-dashboard & git fetch origin & git reset --hard origin/gh-pages
 echo.
 
 echo ######################Step3  view status
