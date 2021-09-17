59dd997665648fd4e13c6268ede6d698b198cb89
Daryl <25836646+srk24@users.noreply.github.com>
2021-09-17 11:58:22 +0800

    Update location-with-script.js

--- quanx/location-with-script.js
+++ quanx/location-with-script.js
@@ -1,19 +1,20 @@
 /**
  * 
- * @author daryl
- * @version 0.2
+ * @author srk24
+ * @version 0.3
  * 
  * @supported http://ip-api.com/json
  * @sample https://raw.githubusercontent.com/crossutility/Quantumult-X/master/sample-location-with-script.js
  */
 
-if ($response.statusCode !== 200) $done(Null)
+if ($response.statusCode != 200) {
+    $done(Null);
+}
 
 const obj = JSON.parse($response.body)
-let title = obj['city']
-let subtitle = obj['isp']
-let ip = obj['query']
-let description = obj['query'] + '\n' + obj['country'] + '\n' + obj['as'] + '\n' + obj['timezone']
-subtitle = subtitle.replace('Hong Kong Telecommunications (HKT)', 'HKT')
+const title = obj['city']
+const subtitle = obj['isp']
+const ip = obj['query']
+const description = obj['query'] + '\n' + obj['country'] + '\n' + obj['as'] + '\n' + obj['timezone']
 
 $done({ title, subtitle, ip, description });
