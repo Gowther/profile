bd363c516070bdda93226964635edb4c2dd41858
Daryl <25836646+srk24@users.noreply.github.com>
2021-09-17 18:21:09 +0800

    Update location-with-script.js

--- quanx/location-with-script.js
+++ quanx/location-with-script.js
@@ -2,15 +2,15 @@
  * @supported http://ip-api.com/json
  * @sample https://raw.githubusercontent.com/crossutility/Quantumult-X/master/sample-location-with-script.js
  *
- * geo_location_checker=http://ip-api.com/json?fields=4203280, https://raw.githubusercontent.com/srk24/profile/master/quanx/location-with-script.js
+ * geo_location_checker=http://ip-api.com/json?fields=11024, https://raw.githubusercontent.com/srk24/profile/master/quanx/location-with-script.js
  */
 
 if ($response.statusCode != 200) $done(Null);
 
 const obj = JSON.parse($response.body)
 const title = obj['city']
-const subtitle = obj['asname']
+const subtitle = obj['isp']
 const ip = obj['query']
-const description = ip + '\n' + obj['timezone'] + '\n' + obj['isp']
+const description = ip + '\n' + obj['timezone'] + '\n' + obj['as']
 
 $done({ title, subtitle, ip, description });
