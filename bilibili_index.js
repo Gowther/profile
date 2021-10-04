d5e86ceb249189c23a06cab103d2923829e80e6f
Wayne <25836646+srk24@users.noreply.github.com>
2021-10-04 15:31:49 +0800

    Create bilibili_index.js

---
+++ bilibili_index.js
@@ -0,0 +1,16 @@
+const url = $request.url
+let body = $response.body
+body = JSON.parse(body)
+	
+
+if (url.includes('feed/index')) {
+	    body['data']['items'].forEach((element, index) => {
+	        if (element.hasOwnProperty('ad_info') || element.hasOwnProperty('banner_item') || element['card_type'] != 'small_cover_v2') {
+	            body['data']['items'].splice(index, 1)
+	        }
+	    })
+}
+
+body = JSON.stringify(body)
+$done({ body })
+
